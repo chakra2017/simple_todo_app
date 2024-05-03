e5c05def9f4c9a0b621606ae6859cd82ee748b64
chakra2017 <bhattacharjee.chakra17@gmail.com>
2024-05-03 22:27:13 +0530

    added BE

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 
 const app = express();
 
 app.use(express.json());
 app.get("/todos", function (req, res) {});
 
-app.post("/todo", function (req, res) {});
+app.post("/todo", function (req, res) {
+  const createPayload = req.body;
+  const parsePayload = createTodo.safeParse(createPayload);
+  if (!parsePayload.success) {
+    res.status(411).json({
+      msg: "wrong input",
+    });
+    return;
+  }
+});
 
-app.post("/completed", function (req, res) {});
+app.post("/completed", function (req, res) {
+  const updatePayload = req.body;
+  const parsePayload = updateTodo.safeParse(updatePayload);
+  if (!parsePayload.success) {
+    res.status(411).json({
+      msg: "wrong input",
+    });
+    return;
+  }
+});
 
 module.exports = app;
